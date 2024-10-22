677e0cca38c4b880714723f76a2d6ab3529403a4
Jesus Alfredo <Jesus Alfredo@GGalfa>
2024-10-21 20:45:19 -0500

    se agregaron funciones en el controller de descuento, modificaciones en la clase descuento y en la vista de descuentos

--- src/application/Controller_descuentos.java
+++ src/application/Controller_descuentos.java
@@ -1,10 +1,15 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Cursor;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -22,6 +27,27 @@ public class Controller_descuentos {
 
 	@FXML
 	private Label Etiqueta_usuarios;
+
+	@FXML
+	private Button btn_agregarcupon;
+
+	@FXML
+	private Button btn_cancelar;
+
+	@FXML
+	private TextField textf_codigocupon;
+
+	@FXML
+	private Label txt_historialcupones;
+
+	@FXML
+	private TextField txtf_fechadevalidezini;
+
+	@FXML
+	private TextField txtf_fechavalidezfin;
+
+	@FXML
+	private TextField txtf_porcentajedescuento;
 
 	@FXML
 	void Cambiar_mouse(MouseEvent event) {
@@ -46,9 +72,10 @@ public class Controller_descuentos {
 		Stage currentStage = (Stage) Etiqueta_descuentos.getScene().getWindow();
 		currentStage.close();
 	}
-    @FXML
-    void Cambio_Vistadescuentos(MouseEvent event) {
-    	try {
+
+	@FXML
+	void Cambio_Vistadescuentos(MouseEvent event) {
+		try {
 			Pane root = FXMLLoader.load(this.getClass().getResource("Vista_descuentos.fxml"));
 			Scene scene = new Scene(root);
 			Stage stage = new Stage();
@@ -60,11 +87,11 @@ public class Controller_descuentos {
 		}
 		Stage currentStage = (Stage) Etiqueta_descuentos.getScene().getWindow();
 		currentStage.close();
-    }
+	}
 
-    @FXML
-    void Cambio_Vistanuevaventa(MouseEvent event) {
-    	try {
+	@FXML
+	void Cambio_Vistanuevaventa(MouseEvent event) {
+		try {
 			Pane root = FXMLLoader.load(this.getClass().getResource("Vista_venta.fxml"));
 			Scene scene = new Scene(root);
 			Stage stage = new Stage();
@@ -76,6 +103,38 @@ public class Controller_descuentos {
 		}
 		Stage currentStage = (Stage) Etiqueta_descuentos.getScene().getWindow();
 		currentStage.close();
+	}
+
+	@FXML
+	void agregar_cupon(ActionEvent event) {
+		String codigo = textf_codigocupon.getText();
+		String descuento = txtf_porcentajedescuento.getText();
+		String fechaini = txtf_fechadevalidezini.getText();
+		String fechafin = txtf_fechavalidezfin.getText();
+		if (!codigo.isEmpty() && !descuento.isEmpty() && !fechaini.isEmpty() && !fechafin.isEmpty()) {
+			Descuento nuevo = new Descuento(codigo, descuento, fechaini, fechafin);
+		} else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("Alerta");
+			alert.setHeaderText(null);
+			alert.setContentText("¡Favor de llenar todos los campos!");
+			alert.showAndWait();
+		}
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("INFO");
+		alert.setHeaderText(null);
+		alert.setContentText("¡cupon generado correctamente!");
+		alert.showAndWait();
+
+	}
+	
+	@FXML
+    void Cancelar_cupon(ActionEvent event) {
+		textf_codigocupon.clear();
+		txtf_fechadevalidezini.clear();
+		txtf_fechavalidezfin.clear();
+		txtf_porcentajedescuento.clear();
+
     }
 
 }
--- src/application/Descuento.java
+++ src/application/Descuento.java
@@ -2,13 +2,17 @@ package application;
 
 public class Descuento {
 	private String codigo;
-	private float descuento;
-	private String fecha_Valides;
+	private String descuento;
+	private String fecha_Validesini;
+	private String fecha_Validesfin;
 
-	public Descuento(String codigo, float descuento, String fecha) {
+	public Descuento(String codigo, String descuento, String fechaini, String fechafin) {
 		this.codigo = codigo;
 		this.descuento = descuento;
-		this.fecha_Valides = fecha;
+		this.fecha_Validesini = fechaini;
+		this.fecha_Validesfin = fechafin;
+		
+		
 	}
 
 	public String getCodigo() {
@@ -19,20 +23,28 @@ public class Descuento {
 		this.codigo = codigo;
 	}
 
-	public float getDescuento() {
+	public String getDescuento() {
 		return descuento;
 	}
 
-	public void setDescuento(float descuento) {
+	public void setDescuento(String descuento) {
 		this.descuento = descuento;
 	}
 
-	public String getFecha_Valides() {
-		return fecha_Valides;
+	public String getFecha_Validesini() {
+		return fecha_Validesini;
 	}
 
-	public void setFecha_Valides(String fecha_Valides) {
-		this.fecha_Valides = fecha_Valides;
+	public String getFecha_Validesfin() {
+		return fecha_Validesfin;
+	}
+
+	public void setFecha_Validesfin(String fecha_Validesfin) {
+		this.fecha_Validesfin = fecha_Validesfin;
+	}
+
+	public void setFecha_Validesini(String fecha_Valides) {
+		this.fecha_Validesini = fecha_Valides;
 	}
 
 }
