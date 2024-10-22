be306d76a1bb5d9dad0ccd2a21c96c3ece561104
Jesus Alfredo <Jesus Alfredo@10.0.28.20>
2024-10-21 19:45:01 -0500

    se agregaron imagenes para la vista y clases desceunto, venta

--- src/application/Descuento.java
+++ src/application/Descuento.java
@@ -1,5 +1,38 @@
 package application;
 
 public class Descuento {
+	private String codigo;
+	private float descuento;
+	private String fecha_Valides;
+
+	public Descuento(String codigo, float descuento, String fecha) {
+		this.codigo = codigo;
+		this.descuento = descuento;
+		this.fecha_Valides = fecha;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public float getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(float descuento) {
+		this.descuento = descuento;
+	}
+
+	public String getFecha_Valides() {
+		return fecha_Valides;
+	}
+
+	public void setFecha_Valides(String fecha_Valides) {
+		this.fecha_Valides = fecha_Valides;
+	}
 
 }
---
+++ src/application/Venta.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.List;
+
+public class Venta {
+	private List<Libro> lista_libros;
+	private int cantidad;
+	private float subtotal;
+	
+	public Venta(List<Libro> lista,int cantidad,float subtotal) {
+		this.cantidad = cantidad;
+		this.lista_libros = lista;
+		this.subtotal = subtotal;
+	}
+
+	public List<Libro> getLista_libros() {
+		return lista_libros;
+	}
+
+	public void setLista_libros(List<Libro> lista_libros) {
+		this.lista_libros = lista_libros;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public float getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(float subtotal) {
+		this.subtotal = subtotal;
+	}
+}
